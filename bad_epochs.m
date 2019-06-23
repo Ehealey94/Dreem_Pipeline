07140c239d2eaa557d837c41784c261c9caaa309
bejachs <bjachs@gmail.com>
2019-06-23 13:54:38 +0100

    Summary
    Added scripts to catch length of each recording
    next:
    1) add a section to capture full meta data, eg onset datetime, length, sampling rate, participant id, meditation type

--- bad_epochs.m
+++ bad_epochs.m
@@ -1,10 +1,12 @@
-function [EEG,RejTrials]=bad_epochs(path)
+function EEG=bad_epochs(path)
 
-inpath=[path '2channels/'];
-outpath=[path 'rej_by_epochs/'];
+%[EEG, RejTrials]=bad_epochs(path)
+
+inpath=[path '4-epoch8secs/'];
+outpath=[path '5-rej_epoch/'];
 
 cd (inpath)
-files=dir('*2chan.set')
+files=dir('*8sec.set')
 
 for i=1:length(files)
     filename=files(i).name
@@ -14,16 +16,16 @@ for i=1:length(files)
     RejTrials(1,i)=EEG.trials;
     opts.reject = 1; opts.recon = 0;
     opts.threshold = 1; opts.slope = 0;
-    try
-        [EEG,rejtrialcount] = preprocess_manageBadTrials(EEG,opts);
-        RejTrials(2,i)=rejtrialcount;
-        
+    %try
+        %[EEG,rejtrialcount] = preprocess_manageBadTrials(EEG,opts);
+        %RejTrials(2,i)=rejtrialcount;
+        EEG = preprocess_manageBadTrials(EEG,opts);
         EEG = pop_saveset( EEG, 'filename',[name '_rej_epoch.set'],'filepath', outpath);
-        RejTrials(3,i)=EEG.trials
-    catch
-        RejTrials(2,i)=EEG.trials;
-        fprintf(['error in file ' filename])
-    end
+        %RejTrials(3,i)=EEG.trials
+%     catch
+%         RejTrials(2,i)=EEG.trials;
+%         fprintf(['error in file ' filename])
+%     end
     
 end
 end
